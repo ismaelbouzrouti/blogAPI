06ac7f6431a798d3cd0a807bb8f63208e8e18ed5
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2024-01-15 07:58:07 +0100

    feat(index.js): starting server + use posts routes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 const db = require('./models/db');
+
+const express = require('express');
+const postsRouter = require('./routes/posts');
+
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/posts', postsRouter);
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
